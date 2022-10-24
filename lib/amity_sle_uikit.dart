6253e893d8486b077ae8959add407ec7b32743ee
Au Thanakorn <92154391+AuThanakorn@users.noreply.github.com>
2022-10-24 22:53:32 +0700

    _fix user profile

--- lib/amity_sle_uikit.dart
+++ lib/amity_sle_uikit.dart
@@ -4,7 +4,6 @@ import 'dart:developer';
 
 import 'package:amity_sdk/amity_sdk.dart';
 import 'package:amity_uikit_beta_service/utils/navigation_key.dart';
-import 'package:amity_uikit_beta_service/viewmodel/follower_following_viewmodel.dart';
 import 'package:amity_uikit_beta_service/viewmodel/notification_viewmodel.dart';
 import 'package:amity_uikit_beta_service/viewmodel/pending_request_viewmodel.dart';
 import 'package:flutter/material.dart';
